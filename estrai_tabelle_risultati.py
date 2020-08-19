3481a19a93a773266b31b4cb011c11724c885047
carlocabras21 <karlokabras@gmail.com>
2020-08-19 15:46:28 +0200

    script finale estrazione tabelle relazione

--- estrai_tabelle_risultati.py
+++ estrai_tabelle_risultati.py
@@ -16,24 +16,25 @@ for array_cartelle in get_folders_list():
 	# la struttura delle liste di array di cartelle è la stessa spiegata in crea_istanze_benchmark
 	
 	current_test = get_tests_list()[test_index]
+	print (current_test)
 	
-	s = "\\begin{longtable}{|l|l|l|l|l|l|}" + "\n" + \
-		"\\multicolumn{6}{c}" + "\n" + \
-		"{{ \\tablename \\hspace{1pt} \\thetable{} }} \\\\" + "\n" + \
-		"\\hline" + "\n" + \
-		"\\multicolumn{6}{|c|}{\\textbf{ " + current_test + "}} \\\\" + "\n" + \
-		"\\hline" + "\n" + \
-		"\\textbf{Istanza} & \\textbf{n locazioni} & \\textbf{n utenti} & \\textbf{t. discreto} & \\textbf{t. continuo} & \\textbf{gap} \\\\" + "\n" + \
-		"\\endfirsthead" + "\n" + \
-		"\\multicolumn{6}{c}" + "\n" + \
-		"{{ \\tablename \\hspace{1pt} \\thetable{} -- continuo della pagina precedente}} \\\\ \\hline" + "\n" + \
-		"\\multicolumn{6}{|c|}{\\textbf{ " + current_test + "}} \\\\" + "\n" + \
-		"\\hline" + "\n" + \
-		"\\textbf{Istanza} & \\textbf{n locazioni} & \\textbf{n utenti} & \\textbf{t. discreto} & \\textbf{t. continuo} & \\textbf{gap} \\\\" + "\n" + \
-		"\\endhead" + "\n" + \
-		"\\hline" + "\n"
+	# s = "\\begin{longtable}{|l|l|l|l|l|l|}" + "\n" + \
+		# "\\multicolumn{6}{c}" + "\n" + \
+		# "{{ \\tablename \\hspace{1pt} \\thetable{} }} \\\\" + "\n" + \
+		# "\\hline" + "\n" + \
+		# "\\multicolumn{6}{|c|}{\\textbf{ " + current_test + "}} \\\\" + "\n" + \
+		# "\\hline" + "\n" + \
+		# "\\textbf{Istanza} & \\textbf{n locazioni} & \\textbf{n utenti} & \\textbf{t. discreto} & \\textbf{t. continuo} & \\textbf{gap} \\\\" + "\n" + \
+		# "\\endfirsthead" + "\n" + \
+		# "\\multicolumn{6}{c}" + "\n" + \
+		# "{{ \\tablename \\hspace{1pt} \\thetable{} -- continuo della pagina precedente}} \\\\ \\hline" + "\n" + \
+		# "\\multicolumn{6}{|c|}{\\textbf{ " + current_test + "}} \\\\" + "\n" + \
+		# "\\hline" + "\n" + \
+		# "\\textbf{Istanza} & \\textbf{n locazioni} & \\textbf{n utenti} & \\textbf{t. discreto} & \\textbf{t. continuo} & \\textbf{gap} \\\\" + "\n" + \
+		# "\\endhead" + "\n" + \
+		# "\\hline" + "\n"
 
-	stampa_video_file(s, False, f_log, 1)
+	# stampa_video_file(s, False, f_log, 1)
 	
 	
 	for cartella in array_cartelle:
@@ -44,7 +45,10 @@ for array_cartelle in get_folders_list():
 												and "lst" not in item \
 												and "dat" not in item \
 												and "new" not in item]
-												
+		
+		gap_sum = 0.0
+		time_disc_sum = 0.0
+		time_cont_sum = 0.0
 		file_counter = 0
 		for file in filtered_files:
 			# da qua estraggo il nome e il numero di utenti e locazioni
@@ -72,17 +76,30 @@ for array_cartelle in get_folders_list():
 
 			
 			s = file.replace("_","\_")+" & " +n_locazioni+" & " +n_utenti+" & " +str(time_disc)+" s & " +str(time_cont)+" s & " +"%.6f"%round(gap, 6)+" \\\\ \\hline"
-			stampa_video_file(s, False, f_log, 1)
-			
-			
+			# stampa_video_file(s, False, f_log, 1)
 			
 			file_counter += 1
+			
+			gap_sum += gap
+			time_disc_sum += time_disc
+			time_cont_sum += time_cont
 	
 	test_index += 1
 	
-	s = "\\caption{Tabella dei risultati ottenuti dalle istanze " + current_test + ".}" + "\n" + \
-		"\\label{tab:" + current_test + "}" + "\n" + \
-		"\\end{longtable}" + "\n" 
-	stampa_video_file(s, False, f_log, 1)
+	# print ("time disc tot = ", time_disc_sum, "time disc avg = ", time_disc_sum/file_counter)
+	# print ("time cont tot = ", time_cont_sum, "time cont avg = ", time_cont_sum/file_counter)
+	# print ("gap avg", gap_sum/file_counter)
+	
+	print ("In questa classe di test il tempo medio di calcolo nel caso discreto è pari a " + "%.3f" % round(time_disc_sum/file_counter, 3) + " s" + 
+			" per un totale di " + "%.3f" % time_disc_sum  + 
+			" s, mentre nel rilassamento continuo si hanno " + "%.3f" % round(time_cont_sum/file_counter, 3) + 
+			" s di media per un totale di "  + "%.3f" % time_cont_sum + " s di esecuzione. " + 
+			"Il gap medio è pari a " + "%.6f" % round(gap_sum/file_counter, 6) + ".")
+	
+	
+	# s = "\\caption{Tabella dei risultati ottenuti dalle istanze " + current_test + ".}" + "\n" + \
+		# "\\label{tab:" + current_test + "}" + "\n" + \
+		# "\\end{longtable}" + "\n" 
+	# stampa_video_file(s, False, f_log, 1)
 	
 	
